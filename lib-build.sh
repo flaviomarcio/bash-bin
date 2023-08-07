eee15eeef626cdef1ade768132a1a519f15daa1f
Flavio-Portela <fmspx@hotmail.com>
2023-08-07 01:11:34 -0300

    master: no message

--- lib-build.sh
+++ lib-build.sh
@@ -191,11 +191,10 @@ function mavenBuild()
     __mvn_cmd="mvn help:evaluate -Dexpression=project.build.finalName -q -DforceStdout"
     echY "      - ${__mvn_cmd}"
     __mvn_jar_filter=$(mvn help:evaluate -Dexpression=project.build.finalName -q -DforceStdout)
+    #binary jar file name
     __mvn_jar_source_file="${__mvn_build_src_bin_dir}/${__mvn_jar_filter}.jar"
     echG "      - jar file: ${__mvn_jar_source_file}"
-    #binary jar file name
-    pwd
-    ls -l ${__mvn_build_src_bin_dir}
+    ls -l ${__mvn_jar_source_file}
     read
     if ! [[ -f ${__mvn_jar_source_file} ]]; then
       echY "      jar file: ${__mvn_jar_source_file}"
@@ -209,24 +208,6 @@ function mavenBuild()
       export __func_return="${__func_return} ${__mvn_jar_source_file_new}"
       echC "      - JAR file: ${__mvn_jar_source_file_new}"
     fi
-
-    # export __mvn_jar_source_files=$(find ${__mvn_build_src_bin_dir} -name ${__mvn_jar_filter})
-  
-    # if [[ ${__mvn_jar_source_files} == "" ]]; then
-    #   echR "      ==============================  "
-    #   echR "      ******JAR file not found******  "
-    #   echR "      ******JAR file not found******  "
-    #   echR "      ==============================  "
-    # else
-    #   __mvn_jar_source_files=(${__mvn_jar_source_files})
-    #   for __mvn_jar_source_file in "${__mvn_jar_source_files[@]}"
-    #   do
-    #     __mvn_jar_source_file_new=${__mvn_build_base_dir}/$(basename ${__mvn_jar_source_file})
-    #     mv ${__mvn_jar_source_file} ${__mvn_jar_source_file_new}
-    #     export __func_return="${__func_return} ${__mvn_jar_source_file_new}"
-    #     echC "      - JAR file: ${__mvn_jar_source_file_new}"
-    #   done
-    # fi
   fi
   cd ${__mvn_build_base_dir}
   rm -rf ${__mvn_build_src_dir}
